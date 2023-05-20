25640af51aa10b547302335449b668acd1b64e21
foridislam332 <foridislam991@gmail.com>
2023-05-20 21:09:04 +0600

    hero banner update

--- src/components/HeroSlider.jsx
+++ src/components/HeroSlider.jsx
@@ -11,7 +11,7 @@ import "swiper/css/pagination";
 import { Navigation, Autoplay } from "swiper";
 
 // slider bg image
-import SliderBg from '../assets/images/slider/bg.jpg';
+import SliderBg from '../assets/images/slider/bg.webp';
 
 // slide image
 import Slide1 from '../assets/images/slider/toys-1.png';
@@ -25,7 +25,7 @@ const HeroSlider = () => {
             navigation={true}
             loop={true}
             autoplay={{
-                delay: 4000,
+                delay: 5000,
                 disableOnInteraction: false,
             }}
             modules={[Navigation, Autoplay]}
@@ -49,7 +49,7 @@ const HeroSlider = () => {
                             </Link>
 
                         </div>
-                        <img className="w-72 lg:w-[450px]" src={Slide1} alt="toys" />
+                        <img className="md:h-96 h-[450px]" src={Slide1} alt="toys" />
                     </div>
                 </div>
             </SwiperSlide>
@@ -60,7 +60,7 @@ const HeroSlider = () => {
                     <div className="container flex flex-col-reverse md:flex-row place-items-center justify-between py-16 text-center md:text-left gap-10 md:gap-0">
                         {/* slide content */}
                         <div className='relative z-50 text-white'>
-                            <h1 className='text-4xl lg:text-6xl font-bold lg:leading-[80px]'>Embrace the Future with Robot Toys!</h1>
+                            <h1 className='text-4xl lg:text-6xl font-bold lg:leading-[80px]'>Embrace the Future with best Robot Toys!</h1>
 
                             <p className="mt-5 mb-14 md:max-w-md text-sm md:text-base">From interactive companions to programmable robots, discover a world of robotic fun and learning.</p>
 
@@ -71,7 +71,7 @@ const HeroSlider = () => {
                             </Link>
 
                         </div>
-                        <img className="w-64 lg:w-96" src={Slide2} alt="toys-robot" />
+                        <img className="md:h-96 h-[450px]" src={Slide2} alt="toys-robot" />
                     </div>
                 </div>
             </SwiperSlide>
@@ -93,7 +93,7 @@ const HeroSlider = () => {
                             </Link>
 
                         </div>
-                        <img className="w-72 lg:w-[450px]" src={Slide3} alt="toys" />
+                        <img className="md:h-96 h-[450px]" src={Slide3} alt="toys" />
                     </div>
                 </div>
             </SwiperSlide>
